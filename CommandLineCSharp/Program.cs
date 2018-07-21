673c3a40e19e2ef4a5a317b74cf535e8d1a205ce
Harsh <deadspaceharsh@gmail.com>
2018-07-21 21:40:06 +0530

    Moving on

--- CommandLineCSharp/Program.cs
+++ CommandLineCSharp/Program.cs
@@ -12,6 +12,18 @@ namespace CommandLineCSharp
             Console.WriteLine("First Number");
             int x = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine(x + 2);
+
+            if(x>2){
+                Console.WriteLine("x is greater than 2");
+            } else {
+                Console.WriteLine("Enter a number which is greater than 2");
+                int y = Convert.ToInt32(Console.ReadLine());
+                if(y>2){
+                    Console.WriteLine("Good");
+                }else{
+                    Console.WriteLine("Bad");
+                }
+            }
         }
     }
 }
