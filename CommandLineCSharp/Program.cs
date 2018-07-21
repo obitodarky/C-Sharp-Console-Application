43242da6a8d16ec014dbf000f372d41f7a2e7df4
Harsh <deadspaceharsh@gmail.com>
2018-07-21 22:17:39 +0530

    exception

--- CommandLineCSharp/Program.cs
+++ CommandLineCSharp/Program.cs
@@ -10,19 +10,31 @@ namespace CommandLineCSharps
             Console.ReadLine();
 
             Console.WriteLine("First Number");
-            int x = Convert.ToInt32(Console.ReadLine());
-
-            if(x>2){
-                Console.WriteLine("x + 2 is " + (x+2));
-            } else {
-                Console.WriteLine("Enter a number which is greater than 2");
-                int y = Convert.ToInt32(Console.ReadLine());
-                if(y>2){
-                    Console.WriteLine("Good");
-                }else{
-                    Console.WriteLine("Bad");
+            try
+            {
+                int x = Convert.ToInt32(Console.ReadLine());
+                if (x > 2)
+                {
+                    Console.WriteLine("x + 2 is " + (x + 2));
+                }
+                else
+                {
+                    Console.WriteLine("Enter a number which is greater than 2");
+                    int y = Convert.ToInt32(Console.ReadLine());
+                    if (y > 2)
+                    {
+                        Console.WriteLine("Good");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bad");
+                    }
                 }
             }
+            catch (Exception){
+                Console.WriteLine("Please enter proper number");
+            }
+
         }
     }
 }
